d6f50ed4e6dd01113f2e24c0bb74af34ceb75956
Prilaz <prilazshrestha090@gmail.com>
2025-06-10 16:22:11 +0545

    updated user.js

--- Dhukuti-Backend/models/User.js
+++ Dhukuti-Backend/models/User.js
@@ -1,18 +1,36 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String },
+    address: {
+      street: String,
+      city: String,
+      district: String,
+      province: String,
+      postalCode: String,
+    },
+    role: { type: String, enum: ["customer", "admin"], default: "customer" },
+    avatar: String,
+    isVerified: { type: Boolean, default: false },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
-// Password hashing middleware
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) next();
-  this.password = await bcrypt.hash(this.password, 10);
-});
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
